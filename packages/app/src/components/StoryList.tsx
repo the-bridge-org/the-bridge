5b1402ac6a38e31650424e91dc9c9eb6d4baeb0f
jianing <jianings0617@gmail.com>
2019-12-08 21:06:44 -0800

    Revert "Added story page and config for capacitor"
    This reverts commit 87e6b4f69cefb6b697301713cf39168d7a22273c.

--- packages/app/src/components/StoryList.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { IonContent, IonIcon, IonItem, IonLabel } from "@ionic/react";
-import { Story } from "./Story";
-
-export const StoryList: React.FC = () => (
-  <IonContent>
-    <Story />
-  </IonContent>
-);
--- packages/app/src/pages/Story.tsx
+++ packages/app/src/pages/Story.tsx
@@ -4,38 +4,18 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
-  IonButton,
-  IonIcon,
-  IonRow,
-  IonCol,
 } from "@ionic/react";
-import { add } from "ionicons/icons";
 import React from "react";
-import { StoryList } from "../components/StoryList";
 
 export const Story: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonRow>
-            <IonCol align-self-start>
-              <IonTitle>Story</IonTitle>
-            </IonCol>
-            <IonCol align-self-center size="8"></IonCol>
-            <IonCol align-self-end no-padding>
-              <div>
-                <IonButton slot="icon-only" fill="clear" expand="block">
-                  <IonIcon icon={add} />
-                </IonButton>
-              </div>
-            </IonCol>
-          </IonRow>
+          <IonTitle>Story</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <StoryList />
-      </IonContent>
+      <IonContent />
     </IonPage>
   );
 };
