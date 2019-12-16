5ea26bb8b47fa8399df207cbfb8866f7005eaa36
jianing <jianings0617@gmail.com>
2019-12-16 00:37:17 -0800

    Reused add story modal

--- packages/app/src/pages/Story.tsx
+++ packages/app/src/pages/Story.tsx
@@ -4,24 +4,15 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
-  IonButton,
-  IonIcon,
   IonRow,
   IonCol,
-  IonModal,
 } from "@ionic/react";
 import { add } from "ionicons/icons";
 import React, { useState } from "react";
 import { StoryList } from "../components/StoryList";
-const modalStyle = {
-  width: "15%",
-  right: "15px",
-  bottom: "15px",
-  position: "absolute",
-};
-const buttonStyle = { width: "100%", height: "1000%" };
+import { AddStoryModal } from "../components/AddStoryModal";
+
 export const Story: React.FC = () => {
-  const [showModal, setShowModal] = useState(false);
   return (
     <IonPage>
       <IonHeader>
@@ -32,46 +23,7 @@ export const Story: React.FC = () => {
             </IonCol>
             <IonCol align-self-center size="6"></IonCol>
             <IonCol align-self-end no-padding>
-              <div>
-                <IonModal showBackdrop isOpen={showModal}>
-                  <IonRow>
-                    <IonCol>
-                      <IonButton color="medium" style={buttonStyle}>
-                        Camera
-                      </IonButton>
-                    </IonCol>
-                    <IonCol>
-                      <IonButton color="medium" style={buttonStyle}>
-                        Photo
-                      </IonButton>
-                    </IonCol>
-                  </IonRow>
-                  <IonRow>
-                    <IonCol>
-                      <IonButton color="medium" style={buttonStyle}>
-                        Video
-                      </IonButton>
-                    </IonCol>
-                    <IonCol></IonCol>
-                  </IonRow>
-                  <IonRow>
-                    <IonButton
-                      fill="clear"
-                      onClick={() => setShowModal(false)}
-                      style={modalStyle}
-                    >
-                      cancel
-                    </IonButton>
-                  </IonRow>
-                </IonModal>
-                <IonButton
-                  fill="clear"
-                  slot="icon-only"
-                  onClick={() => setShowModal(true)}
-                >
-                  <IonIcon icon={add} />
-                </IonButton>
-              </div>
+              <AddStoryModal edit={false} />
             </IonCol>
           </IonRow>
         </IonToolbar>
